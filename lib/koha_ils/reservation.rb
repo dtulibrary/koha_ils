107cb55ad27160b38f442ec2e6446175e6cb767a
Ronan McHugh <mchugh.r@gmail.com>
2017-10-05 12:41:16 +0200

    Add branchcode to reservation object

--- lib/koha_ils/reservation.rb
+++ lib/koha_ils/reservation.rb
@@ -2,15 +2,16 @@ module KohaIls
   class Reservation
     include SAXMachine
     element :title
-    element :reservedate, as: :date_reserved do |elem|
-      Date.parse(elem)
-    end
     element :branchname, as: :branch
+    element :branchcode, as: :branch_code
     element :reserve_id
     element :priority
     element :found
     element :borrowernumber, as: :borrower_number
     element :timestamp do |elem|
+      Date.parse(elem)
+    end
+    element :reservedate, as: :date_reserved do |elem|
       Date.parse(elem)
     end
 
